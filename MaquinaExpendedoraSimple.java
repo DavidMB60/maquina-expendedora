ece849fa6788b6788a53995f321232eeca86e882
David Martínez Bardón <davidmb60@gmail.com>
2018-10-26 12:30:03 +0200

    rev1.1 Precio, origen y destino invariables

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -16,12 +16,12 @@ class MaquinaExpendedoraSimple {
      * precio del billete y el origen y destino dados. Se asume que el precio
      * del billete que se recibe es mayor que 0.
      */
-    public MaquinaExpendedoraSimple(int costeBillete, String origen, String destino) {
-        precioBillete = costeBillete;
+    public MaquinaExpendedoraSimple() {
+        precioBillete = 12;
         balanceClienteActual = 0;
         totalDineroAcumulado = 0;
-        estacionOrigen = origen;
-        estacionDestino = destino;
+        estacionOrigen = "León";
+        estacionDestino = "Madrid";
     }
 
     /**
